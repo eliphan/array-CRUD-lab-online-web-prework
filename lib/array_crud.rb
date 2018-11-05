8d18c3348b04ebfa7a32a81730c8c51951a2cb89
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-05 22:28:52 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,11 +7,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(pets, hamster)
-  array.push("hamster")
+  pets.push("hamster")
 end
 
-def add_element_to_start_of_array(array, element)
-  array.unshift("element")
+def add_element_to_start_of_array(pets, bird)
+  pets.unshift("element")
 end
 
 def remove_element_from_end_of_array(array)
